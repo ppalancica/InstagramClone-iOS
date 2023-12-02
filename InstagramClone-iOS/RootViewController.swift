775c504fdc092f680b03fdfb8ba19047f1b0d3a8
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-02 23:38:02 +0200

    Renamed ViewControllerto RootViewController

--- InstagramClone-iOS/RootViewController.swift
+++ InstagramClone-iOS/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  InstagramClone-iOS
 //
 //  Created by Pavel Palancica on 02.12.2023.
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
     }
 }
